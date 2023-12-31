4bc7ce2bcc42470abb6fa133084a5963d3839cdb
Samir Comas Moral <samir.comas1@gmail.com>
2023-12-31 18:38:00 +0100

    navbar after login
    alomejor fatlaria cambiar la fuente de texto y el background-color

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,10 +1,15 @@
 import { Routes } from '@angular/router';
 import { MainPageComponent } from './pages/main-page/main-page.component';
 import { HomePageComponent } from './pages/home-page/home-page.component';
+import { StudentPageComponent } from './pages/student-page/student-page.component';
+import { MapPageComponent } from './pages/map-page/map-page.component';
 
 export const routes: Routes = [
   {path: 'landing-page', component: MainPageComponent},
   {path: 'home-page', component: HomePageComponent},
+  {path: 'student-page', component: StudentPageComponent},
+  {path: 'calendar-page', component: MapPageComponent},
+  {path: 'map-page', component: MapPageComponent},
 
   {path: '**', pathMatch:'full', redirectTo: 'landing-page'}
 ];
