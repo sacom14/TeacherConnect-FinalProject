e83ce9c704521ddaa0a833e6268bb6a0f5a3b28c
Samir Comas Moral <samir.comas1@gmail.com>
2024-01-30 01:16:25 +0100

    adding the footer, in future change and use grid

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,11 +3,12 @@ import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
 import { NavbarComponent } from './shared/header/navbar/navbar.component';
 import { ReactiveFormsModule } from '@angular/forms';
+import { FooterComponent } from './shared/footer/footer.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet, ReactiveFormsModule, NavbarComponent],
+  imports: [CommonModule, RouterOutlet, ReactiveFormsModule, NavbarComponent, FooterComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
